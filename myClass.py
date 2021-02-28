f5f04e88d9ab4dff941fbe3096b4f8884a6bdc35
Donald McCorquodale <d.a.mccorquodale@gmail.com>
2021-02-28 16:13:36 +1100

    test

--- myClass.py
+++ myClass.py
@@ -5,3 +5,27 @@ def addVector (x, y):
     result.append(x[1] + y[1])
     result.append(x[2] + y[2])
     print (result)
+
+def subtractVector(x, y):
+    print ("subtracting the vectors " + str(x) + " and " + str(y))
+    result = []
+    result.append(x[0] - y[0])
+    result.append(x[1] - y[1])
+    result.append(x[2] - y[2])
+    print (result)
+
+def multiplyVector(x, y):
+    print ("multiplying the vector " + str(x) + " by " + str(y))
+    result = []
+    result.append(x[0] * y)
+    result.append(x[1] * y)
+    result.append(x[2] * y)
+    print (result)
+
+def divideVector(x,y):
+    print ("dividing the vector " + str(x) + " by " + str(y))
+    result = []
+    result.append(vectorB[0] / y)
+    result.append(vectorB[1] / y)
+    result.append(vectorB[2] / y)
+    print (result)
--- testScript.py
+++ testScript.py
@@ -1,1 +1,9 @@
-import myClass
+from myClass import addVector
+from myClass import multiplyVector
+
+vectorA = [1,-3,-2]
+vectorB = [3,6,9]
+
+addVector(vectorA, vectorB)
+
+multiplyVector(vectorA, 7)

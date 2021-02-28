4ddb98bd23052220050fa41551020de831b971b7
Donald McCorquodale <d.a.mccorquodale@gmail.com>
2021-02-28 16:10:05 +1100

    test

---
+++ myClass.py
@@ -0,0 +1,7 @@
+def addVector (x, y):
+    print ("adding the vectors " + str(x) + " and " + str(y))
+    result = []
+    result.append(x[0] + y[0])
+    result.append(x[1] + y[1])
+    result.append(x[2] + y[2])
+    print (result)
